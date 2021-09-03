562dca9d8d6c4d60842c32de65a0414ba16dc111
Jonathan Karr <jonrkarr@gmail.com>
2021-09-02 23:31:01 -0400

    feat: updating to biosimulators-utils 0.1.116

--- biosimulators_cobrapy/__init__.py
+++ biosimulators_cobrapy/__init__.py
@@ -1,7 +1,7 @@
 from ._version import __version__  # noqa: F401
 # :obj:`str`: version
 
-from .core import exec_sed_task, exec_sedml_docs_in_combine_archive  # noqa: F401
+from .core import exec_sed_task, preprocess_sed_task, exec_sed_doc, exec_sedml_docs_in_combine_archive  # noqa: F401
 
 import cobra
 
@@ -9,6 +9,8 @@ __all__ = [
     '__version__',
     'get_simulator_version',
     'exec_sed_task',
+    'preprocess_sed_task',
+    'exec_sed_doc',
     'exec_sedml_docs_in_combine_archive',
 ]
 
--- biosimulators_cobrapy/core.py
+++ biosimulators_cobrapy/core.py
@@ -11,13 +11,13 @@ from .utils import (get_active_objective_sbml_fbc_id, set_simulation_method_arg,
                     apply_variables_to_simulation_method_args, validate_variables, get_results_of_variables)
 from biosimulators_utils.combine.exec import exec_sedml_docs_in_archive
 from biosimulators_utils.config import get_config, Config  # noqa: F401
-from biosimulators_utils.log.data_model import CombineArchiveLog, TaskLog  # noqa: F401
+from biosimulators_utils.log.data_model import CombineArchiveLog, TaskLog, StandardOutputErrorCapturerLevel  # noqa: F401
 from biosimulators_utils.viz.data_model import VizFormat  # noqa: F401
 from biosimulators_utils.report.data_model import ReportFormat, VariableResults, SedDocumentResults  # noqa: F401
 from biosimulators_utils.sedml.data_model import (Task, ModelLanguage, SteadyStateSimulation,  # noqa: F401
                                                   Variable)
 from biosimulators_utils.sedml import validation
-from biosimulators_utils.sedml.exec import exec_sed_doc
+from biosimulators_utils.sedml.exec import exec_sed_doc as base_exec_sed_doc
 from biosimulators_utils.simulator.utils import get_algorithm_substitution_policy
 from biosimulators_utils.utils.core import raise_errors_warnings
 from biosimulators_utils.warnings import warn, BioSimulatorsWarning
@@ -26,11 +26,13 @@ from kisao.data_model import AlgorithmSubstitutionPolicy, ALGORITHM_SUBSTITUTION
 from kisao.utils import get_preferred_substitute_algorithm_by_ids
 from lxml import etree
 import cobra.io
-import functools
+import os
 
 __all__ = [
     'exec_sedml_docs_in_combine_archive',
+    'exec_sed_doc',
     'exec_sed_task',
+    'preprocess_sed_task',
 ]
 
 
@@ -54,20 +56,65 @@ def exec_sedml_docs_in_combine_archive(archive_filename, out_dir, config=None):
             * :obj:`SedDocumentResults`: results
             * :obj:`CombineArchiveLog`: log
     """
-    sed_doc_executer = functools.partial(exec_sed_doc, exec_sed_task)
-    return exec_sedml_docs_in_archive(sed_doc_executer, archive_filename, out_dir,
+    return exec_sedml_docs_in_archive(exec_sed_doc, archive_filename, out_dir,
                                       apply_xml_model_changes=True,
                                       config=config)
 
 
-def exec_sed_task(task, variables, log=None, config=None):
+def exec_sed_doc(doc, working_dir, base_out_path, rel_out_path=None,
+                 apply_xml_model_changes=True,
+                 log=None, indent=0, pretty_print_modified_xml_models=False,
+                 log_level=StandardOutputErrorCapturerLevel.c, config=None):
+    """ Execute the tasks specified in a SED document and generate the specified outputs
+
+    Args:
+        doc (:obj:`SedDocument` or :obj:`str`): SED document or a path to SED-ML file which defines a SED document
+        working_dir (:obj:`str`): working directory of the SED document (path relative to which models are located)
+
+        base_out_path (:obj:`str`): path to store the outputs
+
+            * CSV: directory in which to save outputs to files
+              ``{base_out_path}/{rel_out_path}/{report.id}.csv``
+            * HDF5: directory in which to save a single HDF5 file (``{base_out_path}/reports.h5``),
+              with reports at keys ``{rel_out_path}/{report.id}`` within the HDF5 file
+
+        rel_out_path (:obj:`str`, optional): path relative to :obj:`base_out_path` to store the outputs
+        apply_xml_model_changes (:obj:`bool`, optional): if :obj:`True`, apply any model changes specified in the SED-ML file before
+            calling :obj:`task_executer`.
+        log (:obj:`SedDocumentLog`, optional): log of the document
+        indent (:obj:`int`, optional): degree to indent status messages
+        pretty_print_modified_xml_models (:obj:`bool`, optional): if :obj:`True`, pretty print modified XML models
+        log_level (:obj:`StandardOutputErrorCapturerLevel`, optional): level at which to log output
+        config (:obj:`Config`, optional): BioSimulators common configuration
+        simulator_config (:obj:`SimulatorConfig`, optional): tellurium configuration
+
+    Returns:
+        :obj:`tuple`:
+
+            * :obj:`ReportResults`: results of each report
+            * :obj:`SedDocumentLog`: log of the document
+    """
+    return base_exec_sed_doc(exec_sed_task, doc, working_dir, base_out_path,
+                             rel_out_path=rel_out_path,
+                             apply_xml_model_changes=apply_xml_model_changes,
+                             log=log,
+                             indent=indent,
+                             pretty_print_modified_xml_models=pretty_print_modified_xml_models,
+                             log_level=log_level,
+                             config=config)
+
+
+def exec_sed_task(task, variables, preprocessed_task=None, log=None, config=None):
     ''' Execute a task and save its results
 
     Args:
-       task (:obj:`Task`): task
-       variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
-       log (:obj:`TaskLog`, optional): log for the task
-       config (:obj:`Config`, optional): BioSimulators common configuration
+        task (:obj:`Task`): task
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+        preprocessed_task (:obj:`object`, optional): preprocessed information about the task, including possible
+            model changes and variables. This can be used to avoid repeatedly executing the same initialization
+            for repeated calls to this method.
+        log (:obj:`TaskLog`, optional): log for the task
+        config (:obj:`Config`, optional): BioSimulators common configuration
 
     Returns:
         :obj:`tuple`:
@@ -85,23 +132,29 @@ def exec_sed_task(task, variables, log=None, config=None):
     if config.LOG and not log:
         log = TaskLog()
 
+    if preprocessed_task is None:
+        preprocessed_task = preprocess_sed_task(task, variables, config=config)
+
     model = task.model
     sim = task.simulation
 
     if config.VALIDATE_SEDML:
-        raise_errors_warnings(validation.validate_model_language(task.model.language, ModelLanguage.SBML),
+        raise_errors_warnings(validation.validate_model_language(model.language, ModelLanguage.SBML),
                               error_summary='Language for model `{}` is not supported.'.format(model.id))
-        raise_errors_warnings(validation.validate_model_change_types(task.model.changes, ()),
+        raise_errors_warnings(validation.validate_model_change_types(model.changes, ()),
                               error_summary='Changes for model `{}` are not supported.'.format(model.id))
-        raise_errors_warnings(validation.validate_simulation_type(task.simulation, (SteadyStateSimulation, )),
+        raise_errors_warnings(validation.validate_simulation_type(sim, (SteadyStateSimulation, )),
                               error_summary='{} `{}` is not supported.'.format(sim.__class__.__name__, sim.id))
 
-    target_x_paths_ids = validation.validate_variable_xpaths(
-        variables, task.model.source, attr='id')
-    namespaces = get_namespaces_for_xml_doc(etree.parse(task.model.source))
-    target_x_paths_fbc_ids = validation.validate_variable_xpaths(
+    if not os.path.isfile(model.source):
+        raise FileNotFoundError('Model source `{}` is not a file.'.format(model.source))
+    model_etree = etree.parse(model.source)
+    target_x_paths_ids = validation.validate_target_xpaths(
+        variables, model_etree, attr='id')
+    namespaces = get_namespaces_for_xml_doc(model_etree)
+    target_x_paths_fbc_ids = validation.validate_target_xpaths(
         variables,
-        task.model.source,
+        model_etree,
         attr={
             'namespace': {
                 'prefix': 'fbc',
@@ -112,26 +165,25 @@ def exec_sed_task(task, variables, log=None, config=None):
     )
 
     # Read the model
-    model = cobra.io.read_sbml_model(task.model.source)
+    cobra_model = cobra.io.read_sbml_model(model.source)
 
     # get the SBML-FBC id of the active objective
-    active_objective_fbc_id = get_active_objective_sbml_fbc_id(task.model.source)
+    active_objective_fbc_id = get_active_objective_sbml_fbc_id(model.source)
 
-    # Load the simulation method specified by ``simulation.algorithm``
-    simulation = task.simulation
-    algorithm_kisao_id = simulation.algorithm.kisao_id
+    # Load the simulation method specified by ``sim.algorithm``
+    algorithm_kisao_id = sim.algorithm.kisao_id
     algorithm_substitution_policy = get_algorithm_substitution_policy(config=config)
     exec_kisao_id = get_preferred_substitute_algorithm_by_ids(
-        simulation.algorithm.kisao_id, KISAO_ALGORITHMS_PARAMETERS_MAP.keys(),
+        sim.algorithm.kisao_id, KISAO_ALGORITHMS_PARAMETERS_MAP.keys(),
         substitution_policy=algorithm_substitution_policy)
     method_props = KISAO_ALGORITHMS_PARAMETERS_MAP[exec_kisao_id]
 
     # set up method parameters specified by ``simulation.algorithm.changes``
     method_kw_args = {}
     if exec_kisao_id == algorithm_kisao_id:
-        for method_arg_change in simulation.algorithm.changes:
+        for method_arg_change in sim.algorithm.changes:
             try:
-                set_simulation_method_arg(method_props, method_arg_change, model, method_kw_args)
+                set_simulation_method_arg(method_props, method_arg_change, cobra_model, method_kw_args)
             except NotImplementedError as exception:
                 if (
                     ALGORITHM_SUBSTITUTION_POLICY_LEVELS[algorithm_substitution_policy]
@@ -160,7 +212,7 @@ def exec_sed_task(task, variables, log=None, config=None):
     apply_variables_to_simulation_method_args(target_x_paths_ids, method_props, variables, method_kw_args)
 
     # execute simulation
-    solution = method_props['method'](model, **method_kw_args)
+    solution = method_props['method'](cobra_model, **method_kw_args)
 
     # check that solution was optimal
     if method_props['check_status'] and solution.status != 'optimal':
@@ -168,7 +220,7 @@ def exec_sed_task(task, variables, log=None, config=None):
             solution.status))
 
     if method_props['kisao_id'] in ['KISAO_0000527', 'KISAO_0000528']:
-        solution.objective_value = model.optimize().objective_value
+        solution.objective_value = cobra_model.optimize().objective_value
 
     # Get the results of each variable
     variable_results = get_results_of_variables(target_x_paths_ids, target_x_paths_fbc_ids,
@@ -184,3 +236,21 @@ def exec_sed_task(task, variables, log=None, config=None):
 
     # Return the results of each variable and log
     return variable_results, log
+
+
+def preprocess_sed_task(task, variables, config=None):
+    """ Preprocess a SED task, including its possible model changes and variables. This is useful for avoiding
+    repeatedly initializing tasks on repeated calls of :obj:`exec_sed_task`.
+
+    Args:
+        task (:obj:`Task`): task
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+        preprocessed_task (:obj:`PreprocessedTask`, optional): preprocessed information about the task, including possible
+            model changes and variables. This can be used to avoid repeatedly executing the same initialization for repeated
+            calls to this method.
+        config (:obj:`Config`, optional): BioSimulators common configuration
+
+    Returns:
+        :obj:`object`: preprocessed information about the task
+    """
+    pass
